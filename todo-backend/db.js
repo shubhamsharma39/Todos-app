f9454866326a61bd5db5ebb94c37f28cc4d2aef8
Your Name <your-email@example.com>
2025-07-10 11:15:16 +0530

    Add GitHub Actions workflow and update Dockerfiles

--- todo-backend/db.js
+++ todo-backend/db.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
-const logger = require("./utils/logger")
+const logger = require("./utils/logger"); 
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        logger.info("MongoDB connected!");
-    } catch (error) {
-        logger.error("MongoDB connection failed:", error);
+  try {
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGO_URI not set in environment variables");
     }
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    logger.info(" MongoDB connected!");
+  } catch (error) {
+    logger.error(`MongoDB connection failed: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
